22e8fd557801ba88027afcc174e5cbc67983f69b
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-10-10 19:28:15 -0300

    feat: criado componente card

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  @Input({required: true}) title!: string;
+  @Input({required: true}) description!: string;
+  @Input({required: true}) cardIcon!: string;
+
+}
