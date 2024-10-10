14bdfeb155c9cd3c9bae23f68a2b6c3939276995
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-10-10 19:31:17 -0300

    feat: adicionado rotas no app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { SobreComponent } from './features/sobre/sobre.component';
+import { BuscadorComponent } from './features/buscador/buscador.component';
+import { ContatoComponent } from './features/contato/contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'buscador', component: BuscadorComponent },
+  { path: 'contato', component: ContatoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
