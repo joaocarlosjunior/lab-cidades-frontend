2b8a17453ed9020c8597ebbd6b035ed4ab03ce61
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-10-11 19:47:58 -0300

    refactor: alterado app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { SobreComponent } from './features/sobre/sobre.component';
 import { BuscadorComponent } from './features/buscador/buscador.component';
-import { ContatoComponent } from './features/contato/contato/contato.component';
+import { ContatoComponent } from './features/contato/contato.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'sobre', component: SobreComponent },
-  { path: 'buscador', component: BuscadorComponent },
-  { path: 'contato', component: ContatoComponent }
-];
-
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'contato',
+    loadChildren: () => import('./features/contato/contato.module').then(m => m.ContatoModule)
+  },
+  {
+    path: 'buscador',
+    loadChildren: () => import('./features/buscador/buscador.module').then(m => m.BuscadorModule)
+  },
+  {
+    path: 'sobre',
+    loadChildren: () => import('./features/sobre/sobre.module').then(m => m.SobreModule)
+  }
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
