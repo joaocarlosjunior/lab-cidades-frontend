629b489b3853e6615d3ffec277d908e0f3a0b5d4
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-10-11 19:42:57 -0300

    feat: criado pipe para nome autor

---
+++ src/app/shared/components/card-arquivo/pipes/nome-autor.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Autor } from '../../../model/Autor';
+
+@Pipe({
+  name: 'nomeAutor'
+})
+export class NomeAutorPipe implements PipeTransform {
+
+  transform(autores: Autor[]): string {
+    if (!autores || autores.length === 0) {
+      return '';
+    }
+    if (autores.length === 1) {
+      return autores[0].nomeAutor;
+    }
+    
+    return autores.map(autor => autor.nomeAutor).join(', ');
+  }
+
+}
