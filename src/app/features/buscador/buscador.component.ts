833f421e7848e3fda016b797533442588b47ec3a
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-10-11 19:39:29 -0300

    feat: adicionado chamada a api para buscar arquivos

--- src/app/features/buscador/buscador.component.ts
+++ src/app/features/buscador/buscador.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ArquivoService } from '../../shared/services/arquivo.service';
+import { Arquivo } from '../../shared/model/Arquivo';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-buscador',
   templateUrl: './buscador.component.html',
   styleUrl: './buscador.component.scss'
 })
-export class BuscadorComponent {
-  numbers: number[] = Array.from({ length: 5 }, (_, i) => i + 1);
+export class BuscadorComponent implements OnInit{
+  arquivos!: Observable<Arquivo[]>;
+  searchTriggered = false;
+  arquivosEncontrados = false;
+  contentHeight = '400px';
+  textoDigitado!: string;
+
+  constructor(private arquivoService: ArquivoService){ }
+
+  ngOnInit(): void { }
+
+  setAssunto(assunto: string) {
+    this.searchTriggered = true; // A pesquisa foi disparada
+    this.textoDigitado = assunto;
+    this.arquivos = this.arquivoService.buscarAssunto(assunto);
+
+    this.arquivos.subscribe({
+      next: (data) => {
+        this.arquivosEncontrados = data.length > 0; // Verifica se há arquivos
+        this.contentHeight = this.arquivosEncontrados ? 'auto' : '400px';
+      },
+      error: (err) => {
+        this.arquivosEncontrados = false; // Reseta o estado de arquivos encontrados
+      },
+    });
+  }
 }
