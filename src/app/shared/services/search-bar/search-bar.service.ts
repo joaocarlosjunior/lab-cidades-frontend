1e905c13b042a2d697a80a4e37867d794629ec0e
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-10-25 10:40:55 -0300

    refactor: movido search-bar service

--- src/app/shared/services/search-bar/search-bar.service.ts
+++ src/app/shared/services/search-bar/search-bar.service.ts
@@ -5,7 +5,7 @@ import { Injectable, signal } from '@angular/core';
 })
 export class SearchBarService {
   overlayOpen = signal(false);
-  recentSearches = signal<string[]>(["angular","rxjs","signals","component store"]);
+  recentSearches = signal<string[]>(["teste 01","teste 02","teste 03","teste 04"]);
 
   constructor() {}
 }
