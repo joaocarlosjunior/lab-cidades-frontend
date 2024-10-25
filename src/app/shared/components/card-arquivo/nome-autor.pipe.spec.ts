10e030c1ca863a5c142194d2590dcbd734961ac3
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-10-25 10:25:18 -0300

    remove: removido os arquivos de teste

--- src/app/shared/components/card-arquivo/nome-autor.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NomeAutorPipe } from './pipes/nome-autor.pipe';
-
-describe('NomeAutorPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NomeAutorPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
