96e1e3deb6583be621d83a93f97438309a19f91f
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-10-25 10:35:26 -0300

    feat: criado componente apresentar informação arquivo

---
+++ src/app/shared/components/info-arquivo/info-arquivo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-arquivo',
+  templateUrl: './info-arquivo.component.html',
+  styleUrl: './info-arquivo.component.scss'
+})
+export class InfoArquivoComponent {
+
+}
