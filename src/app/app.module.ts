31d81f3403930d74f28a13b1f03bff3ee6a7c04e
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-11-13 20:37:30 -0300

    feat: adicionado toastr module e provide tradução mat paginator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,32 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { getPortuguesePaginatorIntl } from './shared/functions/getPortuguesePaginatorIntl';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      closeButton: true,
+      preventDuplicates: true,
+      progressBar: true,
+      timeOut: 2000,
+      positionClass: 'toast-top-right',
+    }),
   ],
   providers: [
     provideClientHydration(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    { provide: MatPaginatorIntl, useValue: getPortuguesePaginatorIntl() }
   ],
   bootstrap: [AppComponent]
 })
