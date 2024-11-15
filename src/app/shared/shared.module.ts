c167fca28bc9e1e096ac7214afa959bc350bf08b
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-11-15 01:15:26 -0300

    chore: removido componentes removido da pasta shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,10 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { DataTablesModule } from 'angular-datatables';
 import { CoreModule } from '../core/core.module';
 import { AppMaterialModule } from './app-material/app-material.module';
-import { BuscadorFormComponent } from './components/buscador-form/buscador-form.component';
 import { ButtonPrimaryComponent } from './components/button-primary/button-primary.component';
-import { CardArquivoComponent } from './components/card-arquivo/card-arquivo.component';
-import { NomeAutorPipe } from './components/card-arquivo/pipes/nome-autor.pipe';
 import { CardComponent } from './components/card/card.component';
-import { FilterComponent } from './components/form-filtro/components/filter/filter.component';
-import { FormFiltroComponent } from './components/form-filtro/form-filtro.component';
-import { InfoArquivoComponent } from './components/info-arquivo/info-arquivo.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { SearchOverlayComponent } from './components/search-overlay/search-overlay.component';
 import { ArquivoService } from './services/arquivo.service';
@@ -28,21 +21,14 @@ import { TipoArquivoService } from './services/tipo-arquivo.service';
     CardComponent,
     SearchBarComponent,
     SearchOverlayComponent,
-    CardArquivoComponent,
-    NomeAutorPipe,
     ButtonPrimaryComponent,
-    InfoArquivoComponent,
-    FormFiltroComponent,
-    FilterComponent,
-    BuscadorFormComponent,
   ],
   imports: [
     CommonModule,
     AppMaterialModule,
     CoreModule,
     HttpClientModule,
-    RouterModule,
-    DataTablesModule,
+    RouterModule
   ],
   exports:[
     CommonModule,
@@ -52,11 +38,7 @@ import { TipoArquivoService } from './services/tipo-arquivo.service';
     CoreModule,
     CardComponent,
     SearchBarComponent,
-    CardArquivoComponent,
-    ButtonPrimaryComponent,
-    FormFiltroComponent,
-    BuscadorFormComponent,
-    DataTablesModule
+    ButtonPrimaryComponent
   ],
   providers: [ArquivoService, TipoArquivoService, MesorregiaoService, CidadeService, EstadoService]
 })
