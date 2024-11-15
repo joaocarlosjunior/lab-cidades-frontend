7ec9bf284ffdfd55834cbea27587318fac438acc
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-11-15 01:03:15 -0300

    chore: removido data table

--- src/app/features/dashboard/componentes/arquivo/arquivo.component.ts
+++ src/app/features/dashboard/componentes/arquivo/arquivo.component.ts
@@ -1,34 +1,22 @@
 import {
-  AfterViewChecked,
-  AfterViewInit,
   ChangeDetectorRef,
   Component,
-  OnDestroy,
-  OnInit,
-  ViewChild,
+  OnInit
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import { Subject, take } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 import { Arquivo } from '../../../../core/models/Arquivo';
 import { ArquivoService } from '../../../../shared/services/arquivo.service';
 import { ModalArquivoFormComponent } from './components/modal-arquivo-form/modal-arquivo-form.component';
-import { Config } from 'datatables.net';
-import { ToastrService } from 'ngx-toastr';
-import { DataTableDirective } from 'angular-datatables';
 
 @Component({
   selector: 'app-arquivo',
   templateUrl: './arquivo.component.html',
   styleUrl: './arquivo.component.scss',
 })
-export class ArquivoComponent implements OnInit, OnDestroy, AfterViewInit{
+export class ArquivoComponent implements OnInit{
   arquivoList: Arquivo[] = [];
-  dtOptions: Config = {};
-  dtTrigger: Subject<any> = new Subject<any>();
-  @ViewChild(DataTableDirective, { static: false })
-  dtElement!: DataTableDirective;
-  isViewChecked = false;
 
   constructor(
     private _arquivoService: ArquivoService,
@@ -38,11 +26,6 @@ export class ArquivoComponent implements OnInit, OnDestroy, AfterViewInit{
     private _cdr: ChangeDetectorRef
   ) {}
 
-  ngOnDestroy(): void {
-    if (this.dtTrigger) {
-      this.dtTrigger.unsubscribe();
-    }
-  }
 
   ngOnInit(): void {
   }
