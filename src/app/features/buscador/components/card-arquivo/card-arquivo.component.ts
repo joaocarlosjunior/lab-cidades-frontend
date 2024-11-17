8349cecba80befc520676ab4e946fc9f07c5866c
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-11-17 00:10:30 -0300

    feat: adicionado funcionalidade para adicionar nome do arquivo ao baixar

--- src/app/features/buscador/components/card-arquivo/card-arquivo.component.ts
+++ src/app/features/buscador/components/card-arquivo/card-arquivo.component.ts
@@ -1,52 +1,67 @@
 import { Component, Input } from '@angular/core';
-import { Autor } from '../../../../core/models/Autor';
+import { ToastrService } from 'ngx-toastr';
 import { Arquivo } from '../../../../core/models/Arquivo';
 import { ArquivoService } from '../../../../shared/services/arquivo.service';
 
 @Component({
   selector: 'app-card-arquivo',
   templateUrl: './card-arquivo.component.html',
-  styleUrl: './card-arquivo.component.scss'
+  styleUrl: './card-arquivo.component.scss',
 })
 export class CardArquivoComponent {
-  @Input({ required:true }) arquivosList: Arquivo[] = [];
+  @Input({ required: true }) arquivosList: Arquivo[] = [];
 
-  constructor(private _arquivoService: ArquivoService){}
+  constructor(
+    private _arquivoService: ArquivoService,
+    private _toastr: ToastrService
+  ) {}
 
-  downloadArquivo(arquivoId: number){
+  downloadArquivo(arquivoId: number) {
     if (arquivoId !== null) {
       this._arquivoService.downloadArquivo(arquivoId).subscribe({
         next: (response: any) => {
           const mimeType = response.body?.type || 'application/octet-stream';
           const file = new Blob([response.body!], {
-            type: mimeType
+            type: mimeType,
           });
 
-          const contentDisposition = response.headers.get('Content-Disposition');
-          let fileName = 'downloaded-file';
-          if (contentDisposition) {
-            const match = contentDisposition.match(/filename="?(.+)"?/);
-            if (match) {
-              fileName = match[1];
-            }
-          }
-          
+          const contentDisposition = response.headers.get(
+            'Content-Disposition'
+          );
+
+          const fileName =
+            this.getFilenameFromContentDisposition(contentDisposition);
 
           const blob = window.URL.createObjectURL(file);
 
           const link = document.createElement('a');
           link.href = blob;
-          link.download = fileName;
+          link.download = fileName || 'arquivo';
 
           link.click();
 
           window.URL.revokeObjectURL(blob);
           link.remove();
         },
-        error: () => alert('Erro ao baixar o documento')
+        error: () =>{
+          this._toastr.error('','Erro ao baixar arquivo');
+        }           
       });
-    } else {
-      alert('Insira um ID válido!');
     }
+  }
+
+  private getFilenameFromContentDisposition(
+    contentDisposition: string | null
+  ): string | null {
+    if (!contentDisposition) return null;
+
+    const filenameUtf8Match = /filename\*\s*=\s*UTF-8''(.+)/i.exec(
+      contentDisposition
+    );
+    if (filenameUtf8Match && filenameUtf8Match[1]) {
+      return decodeURIComponent(filenameUtf8Match[1].trim());
+    }
+
+    return null;
   }
 }
