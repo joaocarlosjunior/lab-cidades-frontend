1b60344057b60378a306f29d7cf22cee7333628d
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-11-28 19:22:20 -0300

    feat: adicionado auth guard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+
+  if (typeof window !== 'undefined') {
+    const authToken = sessionStorage.getItem('auth-token');
+
+    if (authToken) {
+      return true; 
+    }
+  }
+
+  router.navigate(['/login']);
+  return false;
+};
