7d7312c0f2c77ce1dd7962f38b5b446a41cb9cad
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-11-28 19:26:58 -0300

    feat: adiconado service no submit

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../../shared/services/login.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   loginForm!: FormGroup;
   error!: string;
 
-  constructor(private _fb: FormBuilder){
-
-  }
+  constructor(
+    private _fb: FormBuilder,
+    private _loginService: LoginService,
+    private toastService: ToastrService,
+    private _router: Router
+  ){}
 
   ngOnInit(): void {
     this.loginForm = this._fb.group({
@@ -22,6 +28,25 @@ export class LoginComponent {
   }
 
   onSubmit(){
-    console.log(this.loginForm.value);
+  if (this.loginForm.valid) {
+    const { login, password } = this.loginForm.value;
+
+    this._loginService.login(login, password).subscribe({
+      next: () =>{
+        this.toastService.success("Login efetuado com sucesso!");
+        this._router.navigate(['/dashboard']);
+      },
+      error: (err) =>{
+        switch(err.error.status){
+          case 401:
+            this.toastService.error(err.error.detail);
+            break;
+          default:
+            this.toastService.error("Erro inesperado! Tente novamente mais tarde");
+            break;
+        }
+      }
+    });
+  }
   }
 }
