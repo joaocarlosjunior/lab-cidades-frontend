2d38dd5d7ec4e1b6c09909873622aa840fbc3e0a
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-11-28 19:28:33 -0300

    feat: adicionado auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -21,12 +22,14 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule)
+    loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
+    canActivate: [authGuard]
   },
   {
     path: 'login',
     loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule)
-  }
+  },
+  { path: '**', redirectTo: 'home' }
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
