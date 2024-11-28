60f586be3c72ede8d3b6d63721d76aee2976023a
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-11-28 19:26:17 -0300

    style: removido espaço

--- src/app/features/login/login-routing.module.ts
+++ src/app/features/login/login-routing.module.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './login.component';
 
 const routes: Routes = [
     { path: '', component: LoginComponent },
-
   ]
   @NgModule({
     imports: [RouterModule.forChild(routes)],
