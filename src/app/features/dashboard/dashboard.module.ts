c59ed256aaab5c40c718b1cb3b97613c4a774cd6
João Carlos Jr <joaocarlos.cic1@gmail.com>
2024-11-28 19:26:02 -0300

    chore: removido CategoriaComponent

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { ArquivoModule } from './componentes/arquivo/arquivo.module';
-import { CategoriaComponent } from './componentes/categoria/categoria.component';
+import { CardInfoDashboardComponent } from './componentes/home-dashboard/components/card-info-dashboard/card-info-dashboard.component';
 import { HomeDashboardComponent } from './componentes/home-dashboard/home-dashboard.component';
 import { LocalidadeComponent } from './componentes/localidade/localidade.component';
 import { SideBarComponent } from './componentes/side-bar/side-bar.component';
 import { TipoArquivoModule } from './componentes/tipo-arquivo/tipo-arquivo.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import { CardInfoDashboardComponent } from './componentes/home-dashboard/components/card-info-dashboard/card-info-dashboard.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    CategoriaComponent,
     LocalidadeComponent,
     SideBarComponent,
     HomeDashboardComponent,
