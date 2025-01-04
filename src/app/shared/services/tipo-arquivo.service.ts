e95f4eaa26774344f6f23ed41fc572189de7cb9e
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-01-04 19:50:04 -0300

    feat: adicionado metodo service tipo arquivo

--- src/app/shared/services/tipo-arquivo.service.ts
+++ src/app/shared/services/tipo-arquivo.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, delay, finalize, Observable } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
 import { ApiResponse } from '../../core/interfaces/ApiResponse';
 import { TipoArquivo } from '../../core/models/TipoArquivo';
@@ -13,11 +13,41 @@ export class TipoArquivoService {
 
   constructor(private httpClient: HttpClient) { }
 
+  carregarTiposArquivo(page: number = 0, size: number = 10): void {
+    this.httpClient
+      .get<ApiResponse<TipoArquivo>>(this.API + '/all', {
+        params: { page, size },
+      })
+  }
+
   listarPaginado(page: number = 0, size: number = 10): Observable<ApiResponse<TipoArquivo>> { 
-    return this.httpClient.get<ApiResponse<TipoArquivo>>(this.API + '/all', { params: { page, size} });
+    return this.httpClient
+    .get<ApiResponse<TipoArquivo>>(this.API + '/all', { params: { page, size} });
   }
 
   list(): Observable<TipoArquivo[]>{
     return this.httpClient.get<TipoArquivo[]>(this.API);
+  }
+
+  criarTipoArquivo(nome_tipo_arquivo: string){
+    return this.httpClient.post<void>(this.API, {nome_tipo_arquivo}).pipe(
+      finalize(() => this.carregarTiposArquivo())
+    );
+  }
+
+  buscarTipoArquivoPeloId(id: number){
+    return this.httpClient.get<TipoArquivo>(`${this.API}/${id}`);
+  }
+
+  editarTipoArquivo(id: number, nome_tipo_arquivo: string){
+    return this.httpClient.put(`${this.API}/${id}`, { nome_tipo_arquivo }).pipe(
+      finalize(() => this.carregarTiposArquivo())
+    );
+  }
+
+  deletarTipoArquivo(id: number){
+    return this.httpClient.delete(`${this.API}/${id}`).pipe(
+      finalize(() => this.carregarTiposArquivo())
+    );
   }
 }
