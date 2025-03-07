2bba6e4aba2d8921056a16cbcb202aada98d6fcf
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-03-07 10:38:02 -0300

    feat: adicionado cadastrar cidade

--- src/app/shared/services/cidade.service.ts
+++ src/app/shared/services/cidade.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { Cidade } from '../../core/models/Cidade';
 import { Observable } from 'rxjs';
+import { RequestCidadeDTO } from '../../core/dtos/RequestCidadeDTO';
+import { DadosSimplesCidade } from '../../core/interfaces/DadosSimplesCidade';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +14,15 @@ export class CidadeService {
 
   constructor(private httpClient: HttpClient) { }
 
-  list(): Observable<Cidade[]> { 
+  list(): Observable<Cidade[]> {
     return this.httpClient.get<Cidade[]>(this.API);
   }
 
-  listaCidadesPeloIdEstado(idEstado: number): Observable<Cidade[]> { 
-    return this.httpClient.get<Cidade[]>(`${this.API}/estado/${idEstado}`);
+  listaCidadesPeloIdEstado(idEstado: number): Observable<DadosSimplesCidade[]> {
+    return this.httpClient.get<DadosSimplesCidade[]>(`${this.API}/estado/${idEstado}`);
+  }
+
+  cadastrarCidade(dadosCidade: RequestCidadeDTO){
+    return this.httpClient.post<void>(this.API, dadosCidade);
   }
 }
