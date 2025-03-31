c00ae588e46112780f40da3d550022935132c354
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-03-30 23:41:43 -0300

    feat: criado pipe para exibir nomes de autores

---
+++ src/app/shared/pipes/autores.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { AutoresPipe } from './autores.pipe';
+
+describe('AutoresPipe', () => {
+  it('create an instance', () => {
+    const pipe = new AutoresPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
